cc60d0010c1d78835afd84aa6816a372f6433100
Oscar Beltran <oscar.beltran@masivian.com>
2020-05-11 01:25:04 -0500

    fix bug

--- js/showquotes.js
+++ js/showquotes.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
 	var queries = queryString.split("&");
 	
 	supplier = {
-		id: queries[0],
-		email: queries[1],
-		name: queries[2]
+		id: queries[0].replace("id=",""),
+		email: queries[1].replace("email=",""),
+		name: queries[2].replace("name=","")
 	}
 
 	var id_quote;
@@ -62,63 +62,13 @@ $(document).ready(function() {
 
 	});
 
-	var fakeResponse = 	[
-		{
-		    "id": 2,
-		    "name": "mobileApp",
-		    "description": "Descripción del producto 1",
-		    "client": {
-		        "id": 1,
-		        "email": "user1@gmail.com",
-		        "password": "123",
-		        "name": "mobileApp",
-		        "profile": "SUPPLIER",
-		        "items": [],
-		        "quotations": [],
-		        "offers": []
-		    },
-		    "offers": null,
-		    "item": null
-		},
-		{
-		    "id": 1,
-		    "name": "mobileApp",
-		    "description": "Descripción del producto 2",
-		    "client": {
-		        "id": 1,
-		        "email": "user1@gmail.com",
-		        "password": "123",
-		        "name": "mobileApp",
-		        "profile": "SUPPLIER",
-		        "items": [],
-		        "quotations": [],
-		        "offers": []
-		    },
-		    "offers": null,
-		    "item": null
-		}
-	]
-
 	var funcionVer = function() {
-
-    
-    $("#tableQuote").empty()
-        	fakeResponse.forEach(function(cotizacion) {
-          	$(`#tableQuote`).append(`<tr>
-            	<td>${cotizacion.id}</td>
-            	<td>${cotizacion.name}</td>
-            	<td>${cotizacion.description}</td>
-            	<td>${cotizacion.client.name}</td>
-            	<td>${cotizacion.client.email}</td>
-            	<td>
-                	<p data-placement="top" data-toggle="tooltip" title="Offer"><button id="quote_${cotizacion.id}" class="btn btn-primary btn-xs offer" data-title="Offer" data-toggle="modal" data-target="#offer" ><span class="glyphicon glyphicon-pencil"></span></button></p>
-              	</td>
-            </tr>`)
-        })
+	var idSupplier = supplier["id"];
+	var url = "http://localhost:8080/quotation/getBySupplier/" + idSupplier;
 
     $.ajax({
-    	"url": "http://localhost:8080/quotation/" + supplier.id,
-      	"method": "get",
+    	"method": "get",
+    	"url": url,
       	"success": function(data) {
     		$("#tableQuote").empty()
         	data.forEach(function(cotizacion) {
